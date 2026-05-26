7d02cf234aaca439ce47a1d99ff213e9eeae87e8
Caio de Paula Colnago <132316409+caiodepc@users.noreply.github.com>
2026-05-26 11:22:57 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,11 @@
 // Define o endereço do registrador PORTB_PCR19 (LED VERDE)
 #define PORTB_PCR19 (*((volatile unsigned int*)0x4004A04C))
 // Define o endereço do registrador GPIOB_PDDR (INPUT/OUTPUT)
-#define GPIOB_PDDR (*((volatile unsigned int*)0x400FF014))
+#define GPIOB_PDDR (*((volatile unsigned int*)0x400FF054))
 // Define o endereço do registrador GPIOB_PSOR (SET)
-#define GPIOB_PSOR (*((volatile unsigned int*)0x400FF004))
+#define GPIOB_PSOR (*((volatile unsigned int*)0x400FF044))
 // Define o endereço do registrador GPIOB_PCOR (CLEAN)
-#define GPIOB_PCOR (*((volatile unsigned int*)0x400FF008)) 
+#define GPIOB_PCOR (*((volatile unsigned int*)0x400FF048)) 
 
 void delayMs (int n) {
 	volatile int i;
@@ -17,28 +17,23 @@ void delayMs (int n) {
 		for (j = 0; j < 7000; j++) {}
 }
 
-int main() {
-
+void main() {
     SIM_SCGC5 |= 0x0400;
 
     // Configurar LED VERDE
     PORTB_PCR19 = 0x0100; 
-
     GPIOB_PDDR |= (1 << 19);
 
     while (1)
     {
-        // Delay
-        delayMs(1000);
-
         // Habilitar saída
         GPIOB_PSOR |= (1 << 19);
-
         // Delay
         delayMs(1000);
 
         // Desabilitar saída
         GPIOB_PCOR |= (1 << 19);
+        // Delay
+        delayMs(1000);
     }
-    return 0;
 }
